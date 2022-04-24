97526c61d8bb4abb202ef4e18bd76c74154193b4
Matias Lires <matilires@gmail.com>
2022-04-24 18:58:21 -0300

    agregando ItemCount al proyecto

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemCount from './components/ItemCount/ItemCount';
+
 
 function App() {
   return (
