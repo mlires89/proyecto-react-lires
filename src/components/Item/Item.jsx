fcaefa8a704462488229a3c64cb87eb758c1e327
Matias Lires <matilires@gmail.com>
2022-05-06 14:16:47 -0300

    modificando css

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,7 +6,7 @@ function Item({item}) {
   return (
     <div className='card'>
         <img className='img-prod'src={item.imgUrl} alt="habano" />
-        <Link className='link-centrado' to={'/item/' + item?.id}> Detalle</Link>
+        <Link className='link-no-decoration' to={'/item/' + item?.id}><button className='button-rounded'> Detalle</button></Link>
         <div className="card-container">
             <h4><b>{item.title}</b></h4>
             <p>$ {item.precio}</p>
